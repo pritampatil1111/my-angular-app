46ddbcc52ec147e12b6c786a9214b37740398bf3
Pritam Patil <pritampatil8056@email.com>
2025-05-10 13:29:01 +0530

    TemplateDrivenForm Added

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-todolist',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.css'
 })
 export class TodolistComponent {
 
+  onSubmit(forms: NgForm)
+  {
+
+  }
 }
